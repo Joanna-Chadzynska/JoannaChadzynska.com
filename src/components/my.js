8b9f74b75db7e12c5df7171b4e9ef6e530698042
Joanna Chądzyńska <jmalek87@gmail.com>
2020-01-17 14:02:34 +0100

    update app

---
+++ src/components/my.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const MyComopnent = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        architecto velit possimus fuga cumque sapiente. Odio sint cum atque
+        ratione, iure quisquam quae odit animi quibusdam similique vero mollitia
+        alias nihil! Laudantium expedita quas perspiciatis sapiente, illo sunt
+        corrupti ea dignissimos excepturi commodi, aspernatur architecto. Sunt
+        minus maiores odio aliquam.
+      </p>
+    </div>
+  )
+}
+
+export default MyComopnent
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <div>
+        <h2>Contact</h2>
+      </div>
+    </Layout>
+  )
+}
+
+export default ContactPage
